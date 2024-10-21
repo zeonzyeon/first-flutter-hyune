0b2204cccff195a58ed0ddac98ef5745b3b02586
Jihyun Jeon <hyuneejj@naver.com>
2024-10-21 23:22:56 +0900

    [사용자 인터페이스 아키텍쳐] StatefulWidget

---
+++ lib/FlutterTask/week4/4-1-2.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Stateful Test'),
+        ),
+        body: MyWidget(),
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyWidgetState();
+  }
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  bool enabled = false;
+  String stateText = "disable";
+
+  void changeCheck() {
+    setState(() {
+      // setState()를 통하여 상태가 변경되었음을 Flutter에 통지 -> build가 재호출되어 UI 재구성
+      if (enabled) {
+        stateText = "disable";
+        enabled = false;
+      } else {
+        stateText = "enable";
+        enabled = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        IconButton(
+          icon: (enabled
+              ? Icon(Icons.check_box, size: 20)
+              : Icon(
+                  Icons.check_box_outline_blank,
+                  size: 20,
+                )),
+          color: Colors.red,
+          onPressed: changeCheck,
+        ),
+        Container(
+          padding: EdgeInsets.only(left: 16),
+          child: Text(
+            '$stateText',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ]),
+    );
+  }
+}
