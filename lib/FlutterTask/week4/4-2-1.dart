e39ffafdeb0f8d33e7690ff507cf3b9e1e9288b1
Jihyun Jeon <hyuneejj@naver.com>
2024-10-22 20:52:44 +0900

    사용자 인터페이스 아키텍쳐(2)

--- lib/FlutterTask/week4/4-2-1.dart
+++ lib/FlutterTask/week4/4-2-1.dart
@@ -10,10 +10,56 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Test'),
+          title: Text('Key Test(StatelessWidget)'),
         ),
-        body: Center(child: GestureDetector(child: Text('HelloWorld'))),
+        body: MyWidget(),
       ),
+    );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyWidgetState();
+  }
+}
+
+class MyColorItemWidget extends StatelessWidget {
+  final Color color;
+  MyColorItemWidget(this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: color,
+        width: 150,
+        height: 150,
+      ),
+    );
+  }
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  List<Widget> widgetList = [
+    MyColorItemWidget(Colors.red),
+    MyColorItemWidget(Colors.blue),
+  ];
+
+  void onChange() {
+    setState(() {
+      widgetList.insert(1, widgetList.removeAt(0));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(children: widgetList),
+        ElevatedButton(onPressed: onChange, child: Text("toggle"))
+      ],
     );
   }
 }
